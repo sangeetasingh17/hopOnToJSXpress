f1e5be257843d56f373465734b2534a074e0cc9d
sangeetasingh17 <sdsinghh17@gmail.com>
2024-01-16 12:16:14 +0530

    add and delete tasks

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,9 +1,41 @@
 import Header from "./components/Header"
+import Tasks from "./components/Tasks"
+import { useState } from 'react'
 
 const App = () => {
+  const [tasks, setTasks] = useState(
+    [
+      {
+        id: 1,
+        text: 'Write Poems',
+        day: 'Jan 15 at 2:00am',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'CP',
+        day: 'Jan 25 at 12:00pm',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'shopping',
+        day: 'Jan 14 at 12:00pm',
+        reminder: true,
+      }
+    ]
+  )
+
+  //delete Task
+  const deleteTask = (id) => {
+    console.log('delete', id)
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
     <div className='container'>
       <Header />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   )
 }
--- react-task-tracker/src/components/Tasks.js
+++ react-task-tracker/src/components/Tasks.js
@@ -1,10 +1,10 @@
-import PropTypes from 'prop-types'
+import Task from './Task'
 
-const Tasks = () => {
+const Tasks = ({ tasks, onDelete}) => {
     return (
-        <div>
-
-        </div>
+        <>
+            {tasks.map((task) => (<Task key={task.id} task={task} onDelete={ onDelete } />))}
+        </>
     )
 }
 
